33e69dd5e9e3803633acfea15ee65a023d7db7cd
Florian Geierstanger <florian@geierstanger.org>
2021-05-19 12:49:41 +0200

    add hamburger svg, rm close button

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,14 +1,19 @@
+const hamburgerButton = document.querySelector('.nav__hamburger');
+
 function openNav() {
 	document.body.classList.add('js-nav--open');
+	console.log(this);
+	hamburgerButton.setAttribute('aria-label', 'close menu');
 	window.addEventListener('keyup', handleKeyUp);
-	document.querySelector('.nav__link').focus();
-	// todo: trap focus in modal
+	document.querySelector('.nav__link').focus(); // set focus to first menu item
+	// TODO: trap focus in modal
 }
 
 function closeNav() {
 	document.body.classList.remove('js-nav--open');
 	window.removeEventListener('keyup', handleKeyUp);
-	document.querySelector('.nav__button-open').focus();
+	hamburgerButton.setAttribute('aria-label', 'open menu');
+	hamburgerButton.focus(); // set focus to hamburger button
 }
 
 function handleKeyUp(e) {
@@ -17,5 +22,6 @@ function handleKeyUp(e) {
 	}
 }
 
-document.querySelector('.nav__button-open').addEventListener('click', openNav);
-document.querySelector('.nav__button-close').addEventListener('click', closeNav);
+hamburgerButton.addEventListener('click', () => {
+	document.body.classList.contains('js-nav--open') ? closeNav() : openNav();
+});
